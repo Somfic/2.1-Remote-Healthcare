bb5017f3d6ef7afb8fa17a1964e07a8b4fcd7310
Lucas <29230467+Somfic@users.noreply.github.com>
2022-09-06 12:48:18 +0200

    Added Bike simulation
    Co-Authored-By: redouan778 <31502288+redouan778@users.noreply.github.com>

--- Remote Healthcare/Data/Providers/Bike/SimulationBikeDataProvider.cs
+++ Remote Healthcare/Data/Providers/Bike/SimulationBikeDataProvider.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace RemoteHealthcare.Data.Providers.Bike;
@@ -5,14 +6,38 @@ namespace RemoteHealthcare.Data.Providers.Bike;
 public class SimulationBikeDataProvider : BikeDataProvider
 {
     private readonly Random _random = new();
-    
+    private readonly Stopwatch _timer = Stopwatch.StartNew();
+    private readonly Stopwatch _totalElapsed = Stopwatch.StartNew();
+
+    private int _totalReset;
+
     public override async Task Process()
     {
+        // Id
         SetId("Simulation");
-        SetSpeed((float)(_random.NextDouble() * 30f));
-        SetDistance(_random.Next(0, 100));
-        SetHeartRate(_random.Next(0, 200));
-        SetElapsed(TimeSpan.FromSeconds(_random.Next(1000)));
+        
+        // Device type
         SetDeviceType(DeviceType.Bike);
+        
+        // Elapsed
+        if (_timer.Elapsed.TotalSeconds >= 64)
+            _timer.Restart();
+        
+        SetElapsed(_timer.Elapsed); // todo: rond af op 0,25 seconden
+        
+        // Total elapsed
+        SetTotalElapsed(_totalElapsed.Elapsed); ;
+        
+        // Speed
+        var deltaSpeed = _random.NextSingle() * 2f - 1f;
+        var newSpeed = GetData().Speed + deltaSpeed;
+        SetSpeed(Math.Clamp(newSpeed, 5, 234));
+        
+        // Distance
+        var newDistance = (GetData().Distance + GetData().Speed * 0.25f) % 256;
+        SetDistance(newDistance);
+        
+        // Heart rate
+        SetHeartRate((int)Math.Round(newSpeed + 80));
     }
 }
