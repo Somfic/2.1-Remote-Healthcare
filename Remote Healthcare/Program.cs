5082fde7aa99a9dfad7eec96a2793d5ffa3b1e84
Richard/NotDutchy <richard.elean@gmail.com>
2022-09-17 23:33:27 +0200

    Solved merge conflict

--- Remote Healthcare/Program.cs
+++ Remote Healthcare/Program.cs
@@ -5,11 +5,7 @@ using RemoteHealthcare.Socket;
 try
 {
     var engine = new EngineConnection();
-<<<<<<< HEAD
     await engine.ConnectAsync("richa");
-=======
-    await engine.ConnectAsync("owena", "abc");
->>>>>>> skybox-time
 
     var bike = await DataProvider.GetBike();
     await bike.Initialise();
