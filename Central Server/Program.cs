23fe9b4542eaef7e60eb16862c053cd744599b5f
Owen Arts <owen.arst@hotmail.com>
2022-09-22 09:50:09 +0200

    Inported the demo code for the server

--- Central Server/Program.cs
+++ Central Server/Program.cs
@@ -1,35 +1,55 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Sockets;
-using RemoteHealthcare.Common.Logger;
 
-var log = new Log(typeof(Program));
-
-try
+namespace Server
 {
-    log.Information("Starting server");
-
-    var ip = IPEndPoint.Parse("127.0.0.1");
-
-    using Socket listener = new(
-        ip.AddressFamily,
-        SocketType.Stream,
-        ProtocolType.Tcp);
-
-    listener.Bind(ip);
-    listener.Listen(100);
-
-    while (true)
+    class Program
     {
-        var client = await listener.AcceptAsync();
-        Task.Run(async () => await HandleClient(client));
-    }
-}
-catch (Exception ex)
-{
-    log.Error(ex, "Could not start server");
-}
+        private static TcpListener listener;
+        private static List<Client> clients = new List<Client>();
 
-async Task HandleClient(Socket client)
-{
-    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Server!");
+
+            listener = new TcpListener(IPAddress.Any, 15243);
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+
+            Console.ReadLine();
+        }
+
+        private static void OnConnect(IAsyncResult ar)
+        {
+            var tcpClient = listener.EndAcceptTcpClient(ar);
+            Console.WriteLine($"Client connected from {tcpClient.Client.RemoteEndPoint}");
+            clients.Add(new Client(tcpClient));
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+        }
+
+        internal static void Broadcast(string packet)
+        {
+            foreach(var client in clients)
+            {
+                client.Write(packet);
+            }
+        }
+
+        internal static void Disconnect(Client client)
+        {
+            clients.Remove(client);
+            Console.WriteLine("Client disconnected");
+        }
+
+        internal static void SendToUser(string user, string packet)
+        {
+            foreach(var client in clients.Where(c => c.UserName == user))
+            {
+                client.Write(packet);
+            }
+        }
+    }
 }
