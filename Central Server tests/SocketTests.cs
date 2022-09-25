4a03642b822cf7b97c2f67b403ba54c9d1f485f5
Lucas <29230467+Somfic@users.noreply.github.com>
2022-09-26 00:00:38 +0200

    Fixed tests

--- Central Server tests/SocketTests.cs
+++ Central Server tests/SocketTests.cs
@@ -11,12 +11,7 @@ public class Tests
     [Test]
     public void Checksum()
     {
-        var checks = new (byte, byte[])[]
-        {
-
-        };
-        
-        var bytes = new byte[] { 0x33, 0x02, 0x04, 0x07 };
+        var bytes = new byte[] { 0x01, 0x02, 0x03, 0x06 };
         
         var sum = bytes.Take(bytes.Length - 1).Sum(x => x);
         var checkByte = BitConverter.GetBytes(sum).First();
