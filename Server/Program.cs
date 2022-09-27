b344ee5898f18f253ccdf75eae6bc186784acb51
Redouan <redouan778@gmail.com>
2022-09-27 16:15:13 +0200

    Cleaned some unused comments

--- Server/Program.cs
+++ Server/Program.cs
@@ -16,7 +16,7 @@ namespace RemoteHealthcare.CentralServer
             listener.Start();
             listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
             Console.ReadLine();
-        }
+        } 
 
         private static void OnConnect(IAsyncResult ar)
         {
