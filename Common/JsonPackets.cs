3e01667d875c8b0efa8d427be4181d93985ceceb
Redouan <redouan778@gmail.com>
2022-09-28 18:39:42 +0200

    Added all the possible JsonPackets for each Opperationcode

--- Common/JsonPackets.cs
+++ Common/JsonPackets.cs
@@ -11,6 +11,79 @@ public abstract class DAbstract
     }
 }
 
+//it the abstract class for eyery Datapacket
+public class DataPacket<T> : DAbstract where T : DAbstract
+{
+    public string OpperationCode;
+    public T data;
+}
+
+//DataPacket is the fundament for the specific packets
+public class DataPacket : DAbstract
+{
+    public string OpperationCode;
+    
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+    
+    private JObject data;
+
+    public T GetData<T>() where T : DAbstract
+    {
+        return this.data.ToObject<T>();
+    }
+}
+    
+public class ErrorPacket : DAbstract
+{
+    public StatusCodes statusCode;
+}
+
+
+public class LoginPacket : DAbstract
+{
+    public string username;
+    public string password;
+}
+
+public class LoginResponsePacket : DAbstract
+{
+    public StatusCodes statusCode;
+    public string message;
+}
+
+public class ChatPacket : DAbstract
+{
+    public string message;
+}
+
+public class ChatResponse : DAbstract
+{
+    public StatusCodes statusCode;
+    public string message;
+}
+
+public class SessionStartPacket : DAbstract
+{
+    //voorlopig leeg laten
+}
+
+public class SessionStartResponse : DAbstract
+{
+    public StatusCodes statusCode;
+    public string message;
+}
+
+public class SessionStopPacket : DAbstract
+{
+    //voorlopig leeg laten
+}
+
+public class SessionStopResponse : DAbstract
+{
+    public StatusCodes statusCode;
+
+    public string message;
+}
 
 /*public class JsonFile
 {
