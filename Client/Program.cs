df3a935ee4028eac2f440c1507a82f60012a935a
NotDutchy <richard.elean@gmail.com>
2022-09-29 12:09:13 +0200

    Modified route with less 90 Degree turns.

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,16 +6,15 @@ using RemoteHealthcare.Data.Providers;
 
 try
 {
-    /*var engine = new EngineConnection();
+    var engine = new EngineConnection();
     await engine.ConnectAsync();
 
     var bike = await DataProvider.GetBike("00472");
-    var heart = await DataProvider.GetHeart();*/
+    var heart = await DataProvider.GetHeart();
+    var vrConnection = new VrConnection(bike, heart, engine);
+    vrConnection.start();
 
-    var client = new Client();
-
-    /*var vrConnection = new VrConnection(bike, heart, engine);
-    vrConnection.start();*/
+    //var client = new Client();
 
     await Task.Delay(-1);
 }
