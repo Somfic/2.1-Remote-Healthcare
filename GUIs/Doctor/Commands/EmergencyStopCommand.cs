6bd7515d4909f8b1bed665fdec943ef6444d2cb9
Redouan <redouan778@gmail.com>
2022-10-18 14:57:18 +0200

    New Branch Commit

--- GUIs/Doctor/Commands/EmergencyStopCommand.cs
+++ GUIs/Doctor/Commands/EmergencyStopCommand.cs
@@ -10,7 +10,7 @@ public class EmergencyStopCommand : BaseCommand
 {
     public EmergencyStopCommand()
     {
-        
+        Console.WriteLine("Nieuw Branch");
     }
 
     public override void Execute(object? parameter)
